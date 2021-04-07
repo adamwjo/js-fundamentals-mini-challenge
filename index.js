a7de0e50885c77d6bd2f829dd24c52eea32eae53
Alex Kravecas <akravecas@yahoo.com>
2021-04-07 15:33:45 -0400

    fifth question done

--- index.js
+++ index.js
@@ -73,12 +73,16 @@ console.log(sameSameButDifferent(123, "122"))
 
 // ***** Question 5 *****
 // *** Uncomment the lines below to test
-// console.log("%cQuestion 5", "color: red")
-// const student1 = { name: "Duane", grade: 88 }
-// updateGrade(student1, 92)
-// console.log(student1)
+console.log("%cQuestion 5", "color: red")
+const student1 = { name: "Duane", grade: 88 }
+
+function updateGrade(x, y){
+  return (x.grade = y)
+}
+updateGrade(student1, 92)
+console.log(student1)
 // // => { name: "Duane", grade: 92 }
-// console.log("%c----------", "color: red")
+console.log("%c----------", "color: red")
 
 
 
